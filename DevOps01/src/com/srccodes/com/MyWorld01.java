e23bfceb24b422aea6a5122e6654dbe3683e51fd
Venkatesh Nayak <venks0101@gmail.com>
2020-04-13 13:15:55 +0530

    my first commit - hello world01 java project.

---
+++ DevOps01/src/com/srccodes/com/MyWorld01.java
@@ -0,0 +1,12 @@
+package com.srccodes.com;
+
+public class MyWorld01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world Project!!");
+
+	}
+
+}
