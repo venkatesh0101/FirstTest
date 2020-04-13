e9c9703faff9e6f0d51b2e9446b425c9a62b4f4a
Venkatesh Nayak <venks0101@gmail.com>
2020-04-13 17:08:08 +0530

    chnaged asre nmade to this

--- DevOps01/src/com/srccodes/com/MyWorld02.java
+++ DevOps01/src/com/srccodes/com/MyWorld02.java
@@ -4,7 +4,7 @@ public class MyWorld02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(" My second class - changed!!");
+System.out.println(" My second class");
 	}
 
 }
