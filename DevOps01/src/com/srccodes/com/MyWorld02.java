e7adfd1bdaf782e9f59a62c424e198a6d8e8ae94
Venkatesh Nayak <venks0101@gmail.com>
2020-04-13 13:26:03 +0530

    My second class file commit and push

---
+++ DevOps01/src/com/srccodes/com/MyWorld02.java
@@ -0,0 +1,10 @@
+package com.srccodes.com;
+
+public class MyWorld02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" My second class");
+	}
+
+}
